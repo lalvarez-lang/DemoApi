6e223ef05aa27f7d6ca9ec4dd60c767beaf59c89
3sneider <dubier1992@gmail.com>
2025-09-10 19:01:01 -0500

    code changes

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,5 +1,10 @@
 namespace DemoApi.Models
 {
+    public class PedidoDto
+    {        
+        public DateTime Fecha { get; set; } = DateTime.UtcNow; // Fecha
+    }
+
     public class Pedido
     {
         public Guid Id { get; set; } = Guid.NewGuid(); // UUID
