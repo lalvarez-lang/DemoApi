394931dceabbed84c69464c006c89eeb2c57dc4a
3sneider <dubier1992@gmail.com>
2025-09-12 19:27:10 -0500

    implementacion kubernetes

--- Program.cs
+++ Program.cs
@@ -14,10 +14,11 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
